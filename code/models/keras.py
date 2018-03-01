93727ef7f1df5f94224ddda20d465e8ff8ffc9c7
Peter Lee <peter.lee@berkeley.edu>
2018-03-01 02:07:07 -0800

    added feature engineering notebook

--- code/models/keras.py
+++ code/models/keras.py
@@ -6,7 +6,3 @@ model.add(Dense(units = 65, activation = 'relu', input_dim = 100))
 model.compile(loss = 'categorical_crosentropy', optimizer = 'sgd', metrix = ['accuracy'])
 model.fit(x_train, y_train, epochs = 5, batch_size = 32)
 model.predict(x_test, batch_size = 128)
-
-
-
-
